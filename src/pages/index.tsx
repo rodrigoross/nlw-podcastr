ccf71c031aed9f015e991535daac67fadc08b26c
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-01-06 09:13:11 -0400

    configurado SSG do Home da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
-export default function Home() {
+// SPA => useEffect dentro da function do Componente => // import { useEffect } from "react";
+//SSR => getServerSideProps. OBS: Carrega e executa toda vez que a página é acessada.
+//SSG => getStaticProps, precisa do parametro revalidate com o tempo para ser gerado. Só é executada em prod.
+
+export default function Home(props) {
+  console.log(props.episodes);
+
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
