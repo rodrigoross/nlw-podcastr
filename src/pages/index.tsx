750133fea90cad5063b4ed4d306409ab91b31f52
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-01-06 14:27:23 -0400

    adiciona Link para roteamento interno da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { api } from "../services/api";
 import { convertDurationToTimeString } from "../util/convertDurationToTimeString";
 
@@ -30,7 +31,6 @@ type HomeProps = {
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
     <div className={styles.homePage}>
-      {/* <h1>Index</h1> */}
       <section className={styles.latestEpisodes}>
         <h2>Ultimos lançamentos</h2>
 
@@ -49,7 +49,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 </div>
 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -94,7 +96,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     </div>
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td>{episode.members}</td>
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
