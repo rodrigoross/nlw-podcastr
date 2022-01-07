c3f65b4a4cee330886acd4b3d16ed21e6c30b7b5
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-01-07 09:39:20 -0400

    refatorado player context para desacoplar de _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,14 @@
-import { useState } from "react";
-
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
 
 import styles from "../styles/app.module.scss";
-import { PlayerContext } from "../contexts/PlayerContext";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setcurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setcurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state:boolean) {
-    setIsPlaying(state)
-  }
-
   return (
-    <PlayerContext.Provider
-      value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}
-    >
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -38,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
